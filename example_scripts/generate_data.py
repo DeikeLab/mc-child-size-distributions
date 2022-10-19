798236fe33423f52c9d48292cc2eab5923371647
Dan Ruth <danjruth@gmail.com>
2022-10-19 16:53:16 -0400

    Update generate_data.py

--- example_scripts/generate_data.py
+++ example_scripts/generate_data.py
@@ -9,14 +9,14 @@ import sizedists.montecarlo as mc
 import numpy as np
 import matplotlib.pyplot as plt
 
-# define the parameters used to create the child size distributions
+# define the parameters describing the distribution of the number of bubbles formed
 m_picker_params = {'m_min':2,'b1':4,'b2':2.3}
-
-# only use 1000 stochastic break-up simulations for each
-n_MC = 1000
 
 # define 4 parent bubble sizes at which to compute the child size distribution
 Delta_vec = np.geomspace(0.1,100,4) 
+
+# only use 1000 stochastic break-up simulations for each parent bubble size
+n_MC = 1000
 
 # create the object to handle the generation of the data with which to 
 dg = mc.DataGeneration(m_picker_params,n_MC=n_MC,Delta_vec=Delta_vec)
